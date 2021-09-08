6b72b350e97442fc2111de9f9204222b2b95e297
Stephan Johansen <stephan.johansen@outlook.com>
2021-09-08 14:12:05 +0200

    Introduces error

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace github_actions_cimple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
         }
     }
 }
